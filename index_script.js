ef7fa03c2d0ef0ce15b85bc21617be637621ec98
qzzwoo <117745588+qzzwoo@users.noreply.github.com>
2023-05-04 07:27:01 +0200

    added low quality javascript

---
+++ index_script.js
@@ -0,0 +1,8 @@
+// Consolas
+
+function logIn() {
+    const username = document.querySelector('#uname').value;
+    const password = document.querySelector('#pass').value;
+    console.log(username)
+    console.log(password)
+}
